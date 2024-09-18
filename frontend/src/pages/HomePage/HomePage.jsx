3ce191d00a19c47830b2bb49132ea9e6ba6e0dce
Phillip McKenzie <pmckenzie6387@gmail.com>
2024-09-18 13:47:13 -0400

    add CSS styling to home

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -5,8 +5,10 @@ import "./HomePage.css";
 export default function HomePage() {
   return (
     <div className="home-page">
-      {/* <Navbar /> */}
-      <div className="form-container">
+      <div id="one">
+        <h1>Get ready for a new adventure!</h1>
+      </div>
+      <div className="form-container" id="two">
         <div className="login-form">
           <form>
             <label>Login</label>
@@ -16,14 +18,28 @@ export default function HomePage() {
             <button type="submit">Sign In</button>
           </form>
         </div>
+        <div className="animation">
+          <image src="/animation/sword_animation.gif" alt="animation" id="sword">ANIMATION IMAGE</image>
+        </div>
         <div className="signup-box">
           <p>To sign up</p>
           <Link to="/signup">Click here!</Link>
         </div>
       </div>
-      <footer>
-        <p>McKenzie Coding © 2024</p>
-        <a href="https://www.linkedin.com/in/your-linkedin-profile">LinkedIn</a>
+      <div id="three">
+      <img src="/images/logo_new.png" id="logo"></img>
+      <p>PROGRESS BAR</p>
+      <h3>Adventure Progress</h3>
+      </div>
+      <footer id="four">
+        <div>
+          <p>McKenzie Coding © 2024</p>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/your-linkedin-profile">
+            LinkedIn
+          </a>
+        </div>
       </footer>
     </div>
   );
